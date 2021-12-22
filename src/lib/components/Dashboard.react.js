16728c3fe5df5dfaf5e518f1cd45a987070752c7
Milla Widell <millawidell@gmail.com>
2021-12-22 17:46:52 +0100

    Sidemenu and appbar

--- src/lib/components/Dashboard.react.js
+++ src/lib/components/Dashboard.react.js
@@ -1,6 +1,33 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
+import Menu from './Menu.react';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import clsx from 'clsx';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { useState } from 'react';
+import { drawerWidth } from './Menu.react';
+
+
+const useStyles = makeStyles( (theme) => ({
+    appBar: {
+        transition: theme.transitions.create(['margin', 'width'], {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+        transition: theme.transitions.create(['margin', 'width'], {
+          easing: theme.transitions.easing.easeOut,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+}))
 
 /**
  * ExampleComponent is an example component.
@@ -9,33 +36,60 @@ import Button from '@material-ui/core/Button';
  * It renders an input with the property `value`
  * which is editable by the user.
  */
-export default class Dashboard extends Component {
-    render() {
-        const {id, label, setProps, value, children} = this.props;
+export default function Dashboard(props) {
+    const {id, label, setProps, value, children} = props;
+    const classes = useStyles();
+    const theme = useTheme();
+    const [open, setOpen] = useState(false);
 
-        return (
-            <div id={id}>
-                <Button variant="contained">TEP Dashboard</Button>
-                <div style={{backgroundColor: "20px", padding: "20px", backgroundColor: "green"}}>{children}</div>
-                What: {label}&nbsp;
-                <input
-                    value={value}
-                    onChange={
-                        /*
-                         * Send the new value to the parent component.
-                         * setProps is a prop that is automatically supplied
-                         * by dash's front-end ("dash-renderer").
-                         * In a Dash app, this will update the component's
-                         * props and send the data back to the Python Dash
-                         * app server if a callback uses the modified prop as
-                         * Input or State.
-                         */
-                        e => setProps({ value: e.target.value })
-                    }
-                />
-            </div>
-        );
-    }
+    const toggle = () => { setOpen(!open) };
+    
+    // TO-DO: "dashboards" should be prop for Dashboard
+    return (
+        <div id={id}>
+            <AppBar
+                position="fixed"
+                className={clsx(classes.appBar, {
+                    [classes.appBarShift]: open,
+                })}
+            >
+                <Toolbar>
+                    <IconButton 
+                    aria-label="menu"
+                    onClick={toggle}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+            <Menu 
+                toggle={toggle}
+                isOpen={open}
+                dashboards={ {
+                    depclean: "DepClean",
+                    vaccinate: "V.A.C.C.I.N.A.T.E",
+            } }>
+            </Menu>
+            <Button variant="contained">TEP Dashboard</Button>
+            <div style={{backgroundColor: "20px", padding: "20px", backgroundColor: "green"}}>{children}</div>
+            What: {label}&nbsp;
+            <input
+                value={value}
+                onChange={
+                    /*
+                        * Send the new value to the parent component.
+                        * setProps is a prop that is automatically supplied
+                        * by dash's front-end ("dash-renderer").
+                        * In a Dash app, this will update the component's
+                        * props and send the data back to the Python Dash
+                        * app server if a callback uses the modified prop as
+                        * Input or State.
+                        */
+                    e => setProps({ value: e.target.value })
+                }
+            />
+        </div>
+    );
 }
 
 Dashboard.defaultProps = {};
---
+++ src/lib/components/Menu.react.js
@@ -0,0 +1,100 @@
+import Drawer from '@material-ui/core/Drawer';
+import IconButton from '@material-ui/core/IconButton';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import React from 'react';
+import Divider from '@material-ui/core/Divider';
+import { List, ListItem } from '@material-ui/core';
+import ListItemText from '@material-ui/core/ListItemText';
+
+export const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    drawerHeader: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+        justifyContent: 'flex-end',
+    },
+}))
+
+/**
+ * ExampleComponent is an example component.
+ * It takes a property, `label`, and
+ * displays it.
+ * It renders an input with the property `value`
+ * which is editable by the user.
+ */
+export default function Menu(props) {
+    const { id, setProps, dashboards, toggle, isOpen } = props;
+    const classes = useStyles();
+    const theme = useTheme();
+
+    console.log(open);
+
+    return (
+        <Drawer
+            className={classes.drawer}
+            anchor="left"
+            open={isOpen}
+            classes={{
+                paper: classes.drawerPaper,
+            }}
+        >
+            <div className={classes.drawerHeader}>
+                <IconButton onClick={toggle}>
+                    {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                </IconButton>
+            </div>
+            <Divider />
+            <List>
+                {Object.keys(dashboards).map(key => (
+                <ListItem button key={key}>
+                    <ListItemText primary={dashboards[key]} />
+                </ListItem>
+            ))}
+            </List>
+        </Drawer>
+    )
+}
+
+Menu.defaultProps = {};
+
+Menu.propTypes = {
+
+    /**
+     * The ID used to identify this component in Dash callbacks.
+     */
+    id: PropTypes.string,
+
+    /**
+     * The list of dashboards displayed in the sidemenu.
+     */
+    dashboards: PropTypes.object,
+
+    /**
+     * Callback to toggle open state.
+     */
+    toggle: PropTypes.func,
+
+    /**
+     * True if menu is open.
+     */
+    isOpen: PropTypes.bool,
+
+    /**
+     * Dash-assigned callback that should be called to report property changes
+     * to Dash, to make them available for callbacks.
+     */
+    setProps: PropTypes.func
+};
